6c82641949c96f275eaad28bd4fd47e3dec3f814
hhanff <hendrik.hanff@googlemail.com>
2017-07-10 21:10:49 +0200

    Added more Qt programs
    Signed-off-by: hhanff <hendrik.hanff@googlemail.com>

---
+++ Qt/calculator/Taschenrechner.cpp
@@ -0,0 +1,11 @@
+//--- Taschenrechner.cpp - start ---
+ 
+#include "Taschenrechner.h"
+ 
+Taschenrechner::Taschenrechner(QMainWindow *parent) : QMainWindow(parent){
+        setupUi(this);
+}
+ 
+Taschenrechner::~Taschenrechner(){
+}
+
---
+++ Qt/calculator/Taschenrechner.h
@@ -0,0 +1,15 @@
+#ifndef TASCHENRECHNER_H
+#define TASCHENRECHNER_H
+ 
+#include "ui_Taschenrechner.h"
+ 
+class Taschenrechner : public QMainWindow, public Ui::MainWindow{
+  Q_OBJECT
+ 
+  public:
+    Taschenrechner (QMainWindow *parent = 0);
+    ~Taschenrechner();
+};
+#endif //TASCHENRECHNER_H
+ 
+//--- Taschenrechner.h - end ---
